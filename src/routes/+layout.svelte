3f23608e36e18fbf9a50e7c54654e87ee9a7767c
Kittihengcharoen Chaemprasert <kittihengcharoen@Kittihengcharoens-MacBook-Pro-2.local>
2026-02-15 17:19:11 +0700

    init: project initialize

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+	import banner from '$lib/assets/est_6.png';
+	
+	import '$lib/styles/global.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>SIT HelloWorld 2026</title>
+	<meta property="og:title" content="SIT HelloWorld 2026" />
+	<meta
+		name="description"
+		content="ยินดีต้อนรับสู่ SIT HelloWorld 2026 ที่นี่มีความลับเจ๋งๆ ของโลกไอทีรอให้พวกเจ้ามาไขปริศนาเพียบ! มีทั้งขุมทรัพย์ความรู้และมิตรภาพ กำลังรอเจ้าอยู่ข้างใน"
+	/>
+	<meta
+		property="og:description"
+		content="ยินดีต้อนรับสู่ SIT HelloWorld 2026 ที่นี่มีความลับเจ๋งๆ ของโลกไอทีรอให้พวกเจ้ามาไขปริศนาเพียบ! มีทั้งขุมทรัพย์ความรู้และมิตรภาพ กำลังรอเจ้าอยู่ข้างใน"
+	/>
+	<meta property="og:image" content={banner} />
+</svelte:head>
+
+{@render children()}
