60d076d8749b366d889fbbd34b52a86757c980b1
Niraphan <niraphan2560@gmail.com>
2026-02-18 19:32:07 +0700

    style: add desktop navbar version shared components

---
+++ src/lib/components/shared/navbar/DesktopNavBar.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+	import wavingModel from '$lib/assets/images/navbar/waving-model.svg?raw';
+	import enrollBtn from '$lib/assets/images/navbar/enroll-btn.svg';
+	import { fade } from 'svelte/transition';
+
+	let isClickWavingModel = $state(false);
+	let timeout: ReturnType<typeof setTimeout>;
+
+	const sections = [
+		{ name: 'หน้าหลัก', section: 'hero' },
+		{ name: 'เกี่ยวกับค่าย', section: 'about' },
+		{ name: 'สาขาในค่าย', section: 'branches' },
+		{ name: 'กำหนดการ', section: 'schedule' }
+	];
+
+	function handleClick() {
+		isClickWavingModel = true;
+
+		clearTimeout(timeout);
+		timeout = setTimeout(() => {
+			isClickWavingModel = false;
+		}, 1500); // ⏱ 2.5 วิ
+	}
+</script>
+
+<nav
+	class="hidden max-w-[1440px] w-[calc(100%-48px)] lg:flex
+         justify-between items-center
+         px-[40px] pr-[20px]
+         border border-[#969696]
+         bg-[rgba(40,40,40,0.79)]
+         backdrop-blur-[12.8px]
+         rounded-full
+         absolute top-[40px]
+         left-1/2 -translate-x-1/2
+         z-50 h-[63px] rela"
+>
+	<div class="flex flex-row items-center gap-[16px] xl:gap-[24px] py-[16px]">
+		{#each sections as section}
+			<button class=" text-[16px] xl:text-[20px] font-medium text-[#FFF]">{section.name}</button>
+		{/each}
+	</div>
+	<div class="relative h-full">
+		<button
+			onclick={handleClick}
+			class="h-full flex items-center
+		       [&>svg]:h-full [&>svg]:w-auto bg-blue-500"
+		>
+			{@html wavingModel}
+		</button>
+		{#if isClickWavingModel}
+			<span
+				class="absolute left-[80px] top-1/2 -translate-y-1/2 ml-3
+                   w-max max-w-[220px] text-start
+                   text-white text-[10px] xl:text-[12px] leading-relaxed"
+				in:fade
+				out:fade
+			>
+				"รู้หรือป่าว! <br class="xl:hidden" />โครงการ Hello World<br />
+				จัดเป็นครั้งที่ <span class="text-[#F3D339] font-semibold">9</span> แล้วนะ!"
+			</span>
+		{/if}
+	</div>
+	<div class="flex flex-row items-center gap-[8px] py-[8px]">
+		<button
+			class="text-[16px] xl:text-[20px] font-medium text-[#FFF] border-2 h-[40px] xl:h-[53px] px-[24px] xl:px-[36px] rounded-full"
+			>ตารางเรียน</button
+		>
+		<div class="relative inline-block h-full">
+			<img src={enrollBtn} alt="Enroll" class="block h-[56px] xl:h-[63px]" />
+			<span
+				class="absolute inset-0 flex items-center justify-center text-[16px] xl:text-[20px] font-medium text-[#391D0C]"
+			>
+				ลงทะเบียน
+			</span>
+		</div>
+	</div>
+</nav>
---
+++ src/lib/components/shared/navbar/NavBar.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import DesktopNavBar from './DesktopNavBar.svelte';
+</script>
+
+<DesktopNavBar />
