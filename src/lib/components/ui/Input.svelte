87364e65b2bc860cd4bca046d224acc25921425d
Thanaphat Boonruck <thanaphatboonrak@hotmail.com>
2026-02-19 16:11:43 +0700

    feat: change error input studentId

--- src/lib/components/ui/Input.svelte
+++ src/lib/components/ui/Input.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { cn } from '$lib/utils/utility-util';
 	import type { Snippet } from 'svelte';
+	import { fade } from 'svelte/transition';
 	import { v7 as UUID } from 'uuid';
 
 	let {
@@ -40,7 +41,7 @@
 		class={cn(
 			'group flex items-stretch w-full border-2 h-[65px] rounded-[25px] overflow-hidden transition-all duration-200',
 			error
-				? 'border-red-500 focus-within:ring-2 focus-within:ring-red-400'
+				? 'border-red-500  bg-red-500/5 focus-within:ring-2 focus-within:ring-red-400'
 				: 'border-gray-300 focus-within:ring-2 focus-within:ring-[#EFC30A] focus-within:border-[#EFC30A]',
 			className
 		)}
@@ -59,9 +60,30 @@
 			{placeholder}
 			class="flex-1 px-4 py-2 bg-transparent focus:outline-none placeholder:opacity-40"
 		/>
+
+		{#if error}
+			<div transition:fade={{ duration: 200 }} class="flex items-center pr-5 text-red-500">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="w-6 h-6"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					><circle cx="12" cy="12" r="10" /><line x1="12" y1="8" x2="12" y2="12" /><line
+						x1="12"
+						y1="16"
+						x2="12.01"
+						y2="16"
+					/></svg
+				>
+			</div>
+		{/if}
 	</div>
 
 	{#if error}
-		<p class="mt-1 text-sm text-red-500">{error}</p>
+		<span class="mt-1 text-sm text-red-500">{error}</span>
 	{/if}
 </div>
--- src/lib/utils/shema.ts
+++ src/lib/utils/shema.ts
@@ -1,19 +1,30 @@
 import { z } from 'zod';
 
 export const formRegister = z.object({
-	email: z.email('อีเมลไม่ถูกต้อง'),
-	studentId: z.string().min(11, 'รหัสนักศึกษาต้องมี 11 หลัก'),
+	email: z.email('รูปแบบอีเมลไม่ถูกต้อง').refine(
+		(val) => {
+			return val.endsWith('@mail.kmutt.ac.th');
+		},
+		{
+			message: 'กรุณาใช้อีเมลนักศึกษา (@mail.kmutt.ac.th)'
+		}
+	),
+	studentId: z
+		.string()
+		.length(11, 'รหัสนักศึกษาต้องมี 11 หลัก')
+		.regex(/^[0-9]/, 'รหัสนักศึกษาตัองเป็นตัวเลข')
+		.refine((val) => val.startsWith('681305'), 'เฉพาะนักศึกษาคณะ SIT (รหัส 681305XXXXX) เท่านั้น'),
 	fullName: z.string().min(2, 'กรุณากรอกชื่อ'),
 	nickname: z.string().min(1, 'กรุณากรอกชื่อเล่น'),
 	phone: z.string().optional(),
 	discord: z.string().optional(),
 	major: z.string().min(1, 'กรุณาเลือกสาขา'),
-	track: z.string().min(1, 'กรุณาเลือกสาย'),
-	question_1: z.string().min(10, 'ต้องตอบข้อความอย่างน้อย 10 ตัวอักษร'),
-	question_2: z.string().min(10, 'ต้องตอบข้อความอย่างน้อย 10 ตัวอักษร'),
-	question_3: z.string().min(10, 'ต้องตอบข้อความอย่างน้อย 10 ตัวอักษร'),
-	question_4: z.string().min(10, 'ต้องตอบข้อความอย่างน้อย 10 ตัวอักษร'),
-	question_5: z.string().min(10, 'ต้องตอบข้อความอย่างน้อย 10 ตัวอักษร')
+	track: z.string().min(1, 'กรุณาเลือกสายที่ต้องการสมัคร'),
+	question_1: z.string().min(30, 'กรุณาอธิบายเหตุผลเพิ่มเติมอีกนิดครับ (อย่างน้อย 30 ตัวอักษร)'),
+	question_2: z.string().min(30, 'กรุณาอธิบายทักษะหรือประสบการณ์ (อย่างน้อย 30 ตัวอักษร)'),
+	question_3: z.string().min(30, 'กรุณาอธิบายวิธีการรับมือปัญหา (อย่างน้อย 30 ตัวอักษร)'),
+	question_4: z.string().min(30, 'กรุณาอธิบายแนวทางการสื่อสาร (อย่างน้อย 30 ตัวอักษร)'),
+	question_5: z.string().min(30, 'กรุณาอธิบายความคาดหวังของน้อง (อย่างน้อย 30 ตัวอักษร)')
 });
 
 export type FormSchema = z.infer<typeof formRegister>;
