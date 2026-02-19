9d787fc22a80ee57ad5be5589fda84aad34d68ea
Niraphan <niraphan2560@gmail.com>
2026-02-19 15:07:08 +0700

    feat: add navbar on layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,12 @@
 	import banner from '$lib/assets/est_6.png';
 
 	import '$lib/styles/global.css';
+	import NavBar from '$lib/components/shared/navbar/NavBar.svelte';
 
 	let { children } = $props();
 </script>
 
+<NavBar />
 <svelte:head>
 	<link rel="icon" href={favicon} />
 	<title>SIT HelloWorld 2026</title>
