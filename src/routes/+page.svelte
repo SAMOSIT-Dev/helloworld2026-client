760708805fc7d145467157868abc64bb1d812c20
Thanaphat Boonruck <thanaphatboonrak@hotmail.com>
2026-02-21 05:14:54 +0700

    feat: add CTA2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import CTA2 from '$lib/components/pages/landing-page/CTA2.svelte';
+</script>
+
+<CTA2></CTA2>
