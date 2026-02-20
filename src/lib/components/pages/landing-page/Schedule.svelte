1b63041d74d8e7535a24ebca2636adeb29e51d6c
Sahapap <92364037+Sahapap1@users.noreply.github.com>
2026-02-21 00:46:26 +0700

    feat : Add schedule on landing page

---
+++ src/lib/components/pages/landing-page/Schedule.svelte
@@ -0,0 +1,339 @@
+<script lang="ts">
+	import Sponsor from '$lib/components/pages/landing-page/Sponsor.svelte';
+</script>
+
+<div class="absolute w-full h-full z-1">
+	<div class="pb-50 inset-0 flex flex-col items-center text-white z-10">
+		<h1
+			class="mt-20 md:mt-30 text-white text-center md:text-[96px] font-medium leading-normal font-['Anuphan'] [text-shadow:0_4px_25.8px_rgba(0,0,0,0.98)]
+			text-[48px]"
+		>
+			กำหนดการ
+		</h1>
+
+			<div class="mt-12 md:w-80% flex md:flex-col">
+				<div
+					class="w-full order-3 md:order-1 flex md:flex-row flex-col justify-around py-13 ml-4 md:justify-between gap-15 md:px-10 text-white text-center text-[16px] font-medium leading-normal font-['Anuphan'] [leading-trim:both] [text-edge:cap]"
+				>
+					<div
+						class="shrink py-2.5 px-3 md:py-3 md:px-10 rounded-[100px] border border-[#969696] bg-[rgba(102,102,102,0.43)] backdrop-blur-[6.4px]"
+					>
+						Base Camp
+					</div>
+					<div
+						class="shrink py-2.5 px-3 md:py-3 md:px-10 rounded-[100px] border border-[#969696] bg-[rgba(102,102,102,0.43)] backdrop-blur-[6.4px]"
+					>
+						The Mystic Forest
+					</div>
+					<div
+						class="shrink py-2.5 px-3 md:py-3 md:px-10 rounded-[100px] border border-[#969696] bg-[rgba(102,102,102,0.43)] backdrop-blur-[6.4px]"
+					>
+						Coming soon
+					</div>
+					<div
+						class="shrink py-2.5 px-3 md:py-3 md:px-10 rounded-[100px] border border-[#969696] bg-[rgba(102,102,102,0.43)] backdrop-blur-[6.4px]"
+					>
+						Coming soon
+					</div>
+					<div
+						class="shrink py-2.5 px-3 md:py-3 md:px-10 rounded-[100px] border border-[#969696] bg-[rgba(102,102,102,0.43)] backdrop-blur-[6.4px]"
+					>
+						Coming soon
+					</div>
+				</div>
+
+				<!-- center -->
+				<div
+					class="flex flex-col md:flex-row gap-10	 md:gap-5 h-full items-center justify-between order-2 py-16 px-2 md:px-25 md:py-2 rounded-[100px] border border-[#969696] bg-[rgba(102,102,102,0.43)] backdrop-blur-[6.4px]"
+				>
+					<div class="relative md:w-11.75 md:h-11.75 ">
+						<svg
+							class="w-full h-full"
+							xmlns="http://www.w3.org/2000/svg"
+							width="47"
+							height="47"
+							viewBox="0 0 47 47"
+							fill="none"
+						>
+							<path
+								d="M2.56746 19.401C2.07055 19.8974 1.67635 20.4869 1.40739 21.1357C1.13844 21.7846 1 22.4801 1 23.1824C1 23.8848 1.13844 24.5803 1.40739 25.2292C1.67635 25.878 2.07055 26.4675 2.56746 26.9639L19.401 43.7974C19.8974 44.2943 20.4869 44.6885 21.1357 44.9575C21.7846 45.2265 22.4801 45.3649 23.1824 45.3649C23.8848 45.3649 24.5803 45.2265 25.2292 44.9575C25.878 44.6885 26.4675 44.2943 26.9639 43.7974L43.7974 26.9639C44.2943 26.4675 44.6885 25.878 44.9575 25.2292C45.2265 24.5803 45.3649 23.8848 45.3649 23.1824C45.3649 22.4801 45.2265 21.7846 44.9575 21.1357C44.6885 20.4869 44.2943 19.8974 43.7974 19.401L26.9639 2.56746C26.4675 2.07055 25.878 1.67635 25.2292 1.40739C24.5803 1.13844 23.8848 1 23.1824 1C22.4801 1 21.7846 1.13844 21.1357 1.40739C20.4869 1.67635 19.8974 2.07055 19.401 2.56746L2.56746 19.401Z"
+								stroke="white"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</svg>
+
+						<svg
+							class="absolute inset-0 m-auto w-6.75 h-6.75"
+							xmlns="http://www.w3.org/2000/svg"
+							width="27"
+							height="27"
+							viewBox="0 0 27 27"
+							fill="none"
+						>
+							<path
+								d="M1.85498 11.0369C1.58394 11.3077 1.36892 11.6292 1.22221 11.9831C1.07551 12.337 1 12.7164 1 13.0995C1 13.4826 1.07551 13.862 1.22221 14.2159C1.36892 14.5698 1.58394 14.8914 1.85498 15.1621L11.0369 24.344C11.3077 24.6151 11.6292 24.8301 11.9831 24.9768C12.337 25.1235 12.7164 25.199 13.0995 25.199C13.4826 25.199 13.862 25.1235 14.2159 24.9768C14.5698 24.8301 14.8914 24.6151 15.1621 24.344L24.3441 15.1621C24.6151 14.8914 24.8301 14.5698 24.9768 14.2159C25.1235 13.862 25.199 13.4826 25.199 13.0995C25.199 12.7164 25.1235 12.337 24.9768 11.9831C24.8301 11.6292 24.6151 11.3077 24.3441 11.0369L15.1621 1.85498C14.8914 1.58394 14.5698 1.36892 14.2159 1.22221C13.862 1.07551 13.4826 1 13.0995 1C12.7164 1 12.337 1.07551 11.9831 1.22221C11.6292 1.36892 11.3077 1.58394 11.0369 1.85498L1.85498 11.0369Z"
+								fill="white"
+								stroke="white"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</svg>
+					</div>
+
+					<div class="w-15 md:w-34.5 border-3 border-white rounded-2xl rotate-90 md:rotate-0"></div>
+
+					<div class="relative w-11.75 h-11.75">
+						<svg
+							class="w-full h-full"
+							xmlns="http://www.w3.org/2000/svg"
+							width="47"
+							height="47"
+							viewBox="0 0 47 47"
+							fill="none"
+						>
+							<path
+								d="M2.56746 19.401C2.07055 19.8974 1.67635 20.4869 1.40739 21.1357C1.13844 21.7846 1 22.4801 1 23.1824C1 23.8848 1.13844 24.5803 1.40739 25.2292C1.67635 25.878 2.07055 26.4675 2.56746 26.9639L19.401 43.7974C19.8974 44.2943 20.4869 44.6885 21.1357 44.9575C21.7846 45.2265 22.4801 45.3649 23.1824 45.3649C23.8848 45.3649 24.5803 45.2265 25.2292 44.9575C25.878 44.6885 26.4675 44.2943 26.9639 43.7974L43.7974 26.9639C44.2943 26.4675 44.6885 25.878 44.9575 25.2292C45.2265 24.5803 45.3649 23.8848 45.3649 23.1824C45.3649 22.4801 45.2265 21.7846 44.9575 21.1357C44.6885 20.4869 44.2943 19.8974 43.7974 19.401L26.9639 2.56746C26.4675 2.07055 25.878 1.67635 25.2292 1.40739C24.5803 1.13844 23.8848 1 23.1824 1C22.4801 1 21.7846 1.13844 21.1357 1.40739C20.4869 1.67635 19.8974 2.07055 19.401 2.56746L2.56746 19.401Z"
+								stroke="white"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</svg>
+
+						<!-- <svg
+							class="absolute inset-0 m-auto w-6.75 h-6.75"
+							xmlns="http://www.w3.org/2000/svg"	
+							width="27"
+							height="27"
+							viewBox="0 0 27 27"
+							fill="none"
+						>
+							<path
+								d="M1.85498 11.0369C1.58394 11.3077 1.36892 11.6292 1.22221 11.9831C1.07551 12.337 1 12.7164 1 13.0995C1 13.4826 1.07551 13.862 1.22221 14.2159C1.36892 14.5698 1.58394 14.8914 1.85498 15.1621L11.0369 24.344C11.3077 24.6151 11.6292 24.8301 11.9831 24.9768C12.337 25.1235 12.7164 25.199 13.0995 25.199C13.4826 25.199 13.862 25.1235 14.2159 24.9768C14.5698 24.8301 14.8914 24.6151 15.1621 24.344L24.3441 15.1621C24.6151 14.8914 24.8301 14.5698 24.9768 14.2159C25.1235 13.862 25.199 13.4826 25.199 13.0995C25.199 12.7164 25.1235 12.337 24.9768 11.9831C24.8301 11.6292 24.6151 11.3077 24.3441 11.0369L15.1621 1.85498C14.8914 1.58394 14.5698 1.36892 14.2159 1.22221C13.862 1.07551 13.4826 1 13.0995 1C12.7164 1 12.337 1.07551 11.9831 1.22221C11.6292 1.36892 11.3077 1.58394 11.0369 1.85498L1.85498 11.0369Z"
+								fill="white"
+								stroke="white"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</svg> -->
+					</div>
+
+					<div class="w-15 md:w-34.5 border-3 border-white rounded-2xl rotate-90 md:rotate-0"></div>
+
+					<div class="relative w-11.75 h-11.75">
+						<svg
+							class="w-full h-full"
+							xmlns="http://www.w3.org/2000/svg"
+							width="47"
+							height="47"
+							viewBox="0 0 47 47"
+							fill="none"
+						>
+							<path
+								d="M2.56746 19.401C2.07055 19.8974 1.67635 20.4869 1.40739 21.1357C1.13844 21.7846 1 22.4801 1 23.1824C1 23.8848 1.13844 24.5803 1.40739 25.2292C1.67635 25.878 2.07055 26.4675 2.56746 26.9639L19.401 43.7974C19.8974 44.2943 20.4869 44.6885 21.1357 44.9575C21.7846 45.2265 22.4801 45.3649 23.1824 45.3649C23.8848 45.3649 24.5803 45.2265 25.2292 44.9575C25.878 44.6885 26.4675 44.2943 26.9639 43.7974L43.7974 26.9639C44.2943 26.4675 44.6885 25.878 44.9575 25.2292C45.2265 24.5803 45.3649 23.8848 45.3649 23.1824C45.3649 22.4801 45.2265 21.7846 44.9575 21.1357C44.6885 20.4869 44.2943 19.8974 43.7974 19.401L26.9639 2.56746C26.4675 2.07055 25.878 1.67635 25.2292 1.40739C24.5803 1.13844 23.8848 1 23.1824 1C22.4801 1 21.7846 1.13844 21.1357 1.40739C20.4869 1.67635 19.8974 2.07055 19.401 2.56746L2.56746 19.401Z"
+								stroke="white"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</svg>
+
+						<!-- <svg
+							class="absolute inset-0 m-auto w-6.75 h-6.75"
+							xmlns="http://www.w3.org/2000/svg"	
+							width="27"
+							height="27"
+							viewBox="0 0 27 27"
+							fill="none"
+						>
+							<path
+								d="M1.85498 11.0369C1.58394 11.3077 1.36892 11.6292 1.22221 11.9831C1.07551 12.337 1 12.7164 1 13.0995C1 13.4826 1.07551 13.862 1.22221 14.2159C1.36892 14.5698 1.58394 14.8914 1.85498 15.1621L11.0369 24.344C11.3077 24.6151 11.6292 24.8301 11.9831 24.9768C12.337 25.1235 12.7164 25.199 13.0995 25.199C13.4826 25.199 13.862 25.1235 14.2159 24.9768C14.5698 24.8301 14.8914 24.6151 15.1621 24.344L24.3441 15.1621C24.6151 14.8914 24.8301 14.5698 24.9768 14.2159C25.1235 13.862 25.199 13.4826 25.199 13.0995C25.199 12.7164 25.1235 12.337 24.9768 11.9831C24.8301 11.6292 24.6151 11.3077 24.3441 11.0369L15.1621 1.85498C14.8914 1.58394 14.5698 1.36892 14.2159 1.22221C13.862 1.07551 13.4826 1 13.0995 1C12.7164 1 12.337 1.07551 11.9831 1.22221C11.6292 1.36892 11.3077 1.58394 11.0369 1.85498L1.85498 11.0369Z"
+								fill="white"
+								stroke="white"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</svg> -->
+					</div>
+
+					<div class="w-15 md:w-34.5 border-3 border-white rounded-2xl rotate-90 md:rotate-0"></div>
+
+					<div class="relative w-11.75 h-11.75">
+						<svg
+							class="w-full h-full"
+							xmlns="http://www.w3.org/2000/svg"
+							width="47"
+							height="47"
+							viewBox="0 0 47 47"
+							fill="none"
+						>
+							<path
+								d="M2.56746 19.401C2.07055 19.8974 1.67635 20.4869 1.40739 21.1357C1.13844 21.7846 1 22.4801 1 23.1824C1 23.8848 1.13844 24.5803 1.40739 25.2292C1.67635 25.878 2.07055 26.4675 2.56746 26.9639L19.401 43.7974C19.8974 44.2943 20.4869 44.6885 21.1357 44.9575C21.7846 45.2265 22.4801 45.3649 23.1824 45.3649C23.8848 45.3649 24.5803 45.2265 25.2292 44.9575C25.878 44.6885 26.4675 44.2943 26.9639 43.7974L43.7974 26.9639C44.2943 26.4675 44.6885 25.878 44.9575 25.2292C45.2265 24.5803 45.3649 23.8848 45.3649 23.1824C45.3649 22.4801 45.2265 21.7846 44.9575 21.1357C44.6885 20.4869 44.2943 19.8974 43.7974 19.401L26.9639 2.56746C26.4675 2.07055 25.878 1.67635 25.2292 1.40739C24.5803 1.13844 23.8848 1 23.1824 1C22.4801 1 21.7846 1.13844 21.1357 1.40739C20.4869 1.67635 19.8974 2.07055 19.401 2.56746L2.56746 19.401Z"
+								stroke="white"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</svg>
+
+						<!-- <svg
+							class="absolute inset-0 m-auto w-6.75 h-6.75"
+							xmlns="http://www.w3.org/2000/svg"	
+							width="27"
+							height="27"
+							viewBox="0 0 27 27"
+							fill="none"
+						>
+							<path
+								d="M1.85498 11.0369C1.58394 11.3077 1.36892 11.6292 1.22221 11.9831C1.07551 12.337 1 12.7164 1 13.0995C1 13.4826 1.07551 13.862 1.22221 14.2159C1.36892 14.5698 1.58394 14.8914 1.85498 15.1621L11.0369 24.344C11.3077 24.6151 11.6292 24.8301 11.9831 24.9768C12.337 25.1235 12.7164 25.199 13.0995 25.199C13.4826 25.199 13.862 25.1235 14.2159 24.9768C14.5698 24.8301 14.8914 24.6151 15.1621 24.344L24.3441 15.1621C24.6151 14.8914 24.8301 14.5698 24.9768 14.2159C25.1235 13.862 25.199 13.4826 25.199 13.0995C25.199 12.7164 25.1235 12.337 24.9768 11.9831C24.8301 11.6292 24.6151 11.3077 24.3441 11.0369L15.1621 1.85498C14.8914 1.58394 14.5698 1.36892 14.2159 1.22221C13.862 1.07551 13.4826 1 13.0995 1C12.7164 1 12.337 1.07551 11.9831 1.22221C11.6292 1.36892 11.3077 1.58394 11.0369 1.85498L1.85498 11.0369Z"
+								fill="white"
+								stroke="white"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</svg> -->
+					</div>
+
+					<div class="w-15 md:w-34.5 border-3 border-white rounded-2xl rotate-90 md:rotate-0"></div>
+
+					<div class="relative w-11.75 h-11.75">
+						<svg
+							class="w-full h-full"
+							xmlns="http://www.w3.org/2000/svg"
+							width="47"
+							height="47"
+							viewBox="0 0 47 47"
+							fill="none"
+						>
+							<path
+								d="M2.56746 19.401C2.07055 19.8974 1.67635 20.4869 1.40739 21.1357C1.13844 21.7846 1 22.4801 1 23.1824C1 23.8848 1.13844 24.5803 1.40739 25.2292C1.67635 25.878 2.07055 26.4675 2.56746 26.9639L19.401 43.7974C19.8974 44.2943 20.4869 44.6885 21.1357 44.9575C21.7846 45.2265 22.4801 45.3649 23.1824 45.3649C23.8848 45.3649 24.5803 45.2265 25.2292 44.9575C25.878 44.6885 26.4675 44.2943 26.9639 43.7974L43.7974 26.9639C44.2943 26.4675 44.6885 25.878 44.9575 25.2292C45.2265 24.5803 45.3649 23.8848 45.3649 23.1824C45.3649 22.4801 45.2265 21.7846 44.9575 21.1357C44.6885 20.4869 44.2943 19.8974 43.7974 19.401L26.9639 2.56746C26.4675 2.07055 25.878 1.67635 25.2292 1.40739C24.5803 1.13844 23.8848 1 23.1824 1C22.4801 1 21.7846 1.13844 21.1357 1.40739C20.4869 1.67635 19.8974 2.07055 19.401 2.56746L2.56746 19.401Z"
+								stroke="white"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</svg>
+
+						<!-- <svg
+							class="absolute inset-0 m-auto w-6.75 h-6.75"
+							xmlns="http://www.w3.org/2000/svg"	
+							width="27"
+							height="27"
+							viewBox="0 0 27 27"
+							fill="none"
+						>
+							<path
+								d="M1.85498 11.0369C1.58394 11.3077 1.36892 11.6292 1.22221 11.9831C1.07551 12.337 1 12.7164 1 13.0995C1 13.4826 1.07551 13.862 1.22221 14.2159C1.36892 14.5698 1.58394 14.8914 1.85498 15.1621L11.0369 24.344C11.3077 24.6151 11.6292 24.8301 11.9831 24.9768C12.337 25.1235 12.7164 25.199 13.0995 25.199C13.4826 25.199 13.862 25.1235 14.2159 24.9768C14.5698 24.8301 14.8914 24.6151 15.1621 24.344L24.3441 15.1621C24.6151 14.8914 24.8301 14.5698 24.9768 14.2159C25.1235 13.862 25.199 13.4826 25.199 13.0995C25.199 12.7164 25.1235 12.337 24.9768 11.9831C24.8301 11.6292 24.6151 11.3077 24.3441 11.0369L15.1621 1.85498C14.8914 1.58394 14.5698 1.36892 14.2159 1.22221C13.862 1.07551 13.4826 1 13.0995 1C12.7164 1 12.337 1.07551 11.9831 1.22221C11.6292 1.36892 11.3077 1.58394 11.0369 1.85498L1.85498 11.0369Z"
+								fill="white"
+								stroke="white"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</svg> -->
+					</div>
+				</div>
+
+				<div class="flex md:flex-row flex-col justify-between mt-11 ml-5 md:mt-5 md:pl-15 md:order-3">
+					<div class="w-32 md:w-44 mb-10 text-right md:text-left pr-6 md:pr-0">
+						<p
+							class="md:mb-6 text-white text-[10px]  md:text-base font-semibold leading-normal font-['Anuphan'] [text-shadow:0_4px_39px_rgba(0,0,0,0.83)] [leading-trim:both] [text-edge:cap]"
+						>
+							1 มีนาคม 2026
+						</p>
+						<p
+							class="md:mb-4.5 text-white text-[16px] md:text-xl font-medium leading-normal font-['Anuphan'] [text-shadow:0_4px_39px_rgba(0,0,0,0.83)] [leading-trim:both] [text-edge:cap]"
+						>
+							วันเปิดค่าย
+						</p>
+						<p
+							class="text-[#969696] text-[10px] md:text-base font-medium leading-5 font-['Anuphan'] [text-shadow:0_4px_39px_rgba(0,0,0,0.83)] [leading-trim:both] [text-edge:cap]"
+						>
+							เริ่มปูพื้นฐาน เตรียมเครื่องมือและทำความรู้จักกัน
+						</p>
+					</div>
+					<div class="w-32 md:w-44 mb-10 text-right md:text-left pr-6 md:pr-0 invisible">
+						<p
+							class="md:mb-6 text-white text-[10px]  md:text-base font-semibold leading-normal font-['Anuphan'] [text-shadow:0_4px_39px_rgba(0,0,0,0.83)] [leading-trim:both] [text-edge:cap]"
+						>
+							1 มีนาคม 2026
+						</p>
+						<p
+							class="md:mb-4.5 text-white text-[16px] md:text-xl font-medium leading-normal font-['Anuphan'] [text-shadow:0_4px_39px_rgba(0,0,0,0.83)] [leading-trim:both] [text-edge:cap]"
+						>
+							วันเปิดค่าย
+						</p>
+						<p
+							class="text-[#969696] text-[10px] md:text-base font-medium leading-5 font-['Anuphan'] [text-shadow:0_4px_39px_rgba(0,0,0,0.83)] [leading-trim:both] [text-edge:cap]"
+						>
+							เริ่มปูพื้นฐาน เตรียมเครื่องมือและทำความรู้จักกัน
+						</p>
+					</div>
+					<div class="w-32 md:w-44 mb-10 text-right md:text-left pr-6 md:pr-0 invisible">
+						<p
+							class="md:mb-6 text-white text-[10px]  md:text-base font-semibold leading-normal font-['Anuphan'] [text-shadow:0_4px_39px_rgba(0,0,0,0.83)] [leading-trim:both] [text-edge:cap]"
+						>
+							1 มีนาคม 2026
+						</p>
+						<p
+							class="md:mb-4.5 text-white text-[16px] md:text-xl font-medium leading-normal font-['Anuphan'] [text-shadow:0_4px_39px_rgba(0,0,0,0.83)] [leading-trim:both] [text-edge:cap]"
+						>
+							วันเปิดค่าย
+						</p>
+						<p
+							class="text-[#969696] text-[10px] md:text-base font-medium leading-5 font-['Anuphan'] [text-shadow:0_4px_39px_rgba(0,0,0,0.83)] [leading-trim:both] [text-edge:cap]"
+						>
+							เริ่มปูพื้นฐาน เตรียมเครื่องมือและทำความรู้จักกัน
+						</p>
+					</div>
+					<div class="w-32 md:w-44 mb-10 text-right md:text-left pr-6 md:pr-0 invisible">
+						<p
+							class="md:mb-6 text-white text-[10px]  md:text-base font-semibold leading-normal font-['Anuphan'] [text-shadow:0_4px_39px_rgba(0,0,0,0.83)] [leading-trim:both] [text-edge:cap]"
+						>
+							1 มีนาคม 2026
+						</p>
+						<p
+							class="md:mb-4.5 text-white text-[16px] md:text-xl font-medium leading-normal font-['Anuphan'] [text-shadow:0_4px_39px_rgba(0,0,0,0.83)] [leading-trim:both] [text-edge:cap]"
+						>
+							วันเปิดค่าย
+						</p>
+						<p
+							class="text-[#969696] text-[10px] md:text-base font-medium leading-5 font-['Anuphan'] [text-shadow:0_4px_39px_rgba(0,0,0,0.83)] [leading-trim:both] [text-edge:cap]"
+						>
+							เริ่มปูพื้นฐาน เตรียมเครื่องมือและทำความรู้จักกัน
+						</p>
+					</div>
+					<div class="w-32 md:w-44 mb-10 text-right md:text-left pr-6 md:pr-0 invisible">
+						<p
+							class="md:mb-6 text-white text-[10px]  md:text-base font-semibold leading-normal font-['Anuphan'] [text-shadow:0_4px_39px_rgba(0,0,0,0.83)] [leading-trim:both] [text-edge:cap]"
+						>
+							1 มีนาคม 2026
+						</p>
+						<p
+							class="md:mb-4.5 text-white text-[16px] md:text-xl font-medium leading-normal font-['Anuphan'] [text-shadow:0_4px_39px_rgba(0,0,0,0.83)] [leading-trim:both] [text-edge:cap]"
+						>
+							วันเปิดค่าย
+						</p>
+						<p
+							class="text-[#969696] text-[10px] md:text-base font-medium leading-5 font-['Anuphan'] [text-shadow:0_4px_39px_rgba(0,0,0,0.83)] [leading-trim:both] [text-edge:cap]"
+						>
+							เริ่มปูพื้นฐาน เตรียมเครื่องมือและทำความรู้จักกัน
+						</p>
+					</div>
+					
+				</div>
+			</div>
+			<button
+				class="cursor-pointer mt-6	 md:mt-22.5 px-19 md:px-30 h-15 md:h-20 text-white text-[20px] md:text-[32px] font-semibold leading-normal font-['Anuphan'] rounded-[100px] border-2 border-[#E3E3E3] bg-[linear-gradient(180deg,rgba(51,51,51,0.46)_3.75%,rgba(168,168,168,0.46)_100%)] backdrop-blur-[6.4px]"
+				>ตารางเรียน</button
+			>
+
+			<!-- <Sponsor /> -->
+	</div>
+</div>
