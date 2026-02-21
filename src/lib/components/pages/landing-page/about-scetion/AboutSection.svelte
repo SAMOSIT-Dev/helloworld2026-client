2603c77c6bce3f526931997e8f2484bf0be586d0
Kittihengcharoen Chaemprasert <kittihengcharoen@Kittihengcharoens-MacBook-Pro-2.local>
2026-02-21 07:09:04 +0700

    feat: add branch description

--- src/lib/components/pages/landing-page/about-scetion/AboutSection.svelte
+++ src/lib/components/pages/landing-page/about-scetion/AboutSection.svelte
@@ -7,16 +7,61 @@
 	import backendIcon from '$lib/assets/images/branch/backend-icon.svg';
 
 	let currentIcon = $state(0);
-	
 
-	const icons: { name: string; icon: string; description: string; fontsize?: string; fontSizeMobile?: string; fontSizeTablet?: string }[] = [
-		{ name: 'UX/UI', icon: uxuiIcon, description: 'UX UI description demo test', fontsize: 'xl:text-[140px]', fontSizeTablet: 'md:text-[100px]', fontSizeMobile: 'text-[30px]' },
-		{ name: 'Database', icon: databaseIcon, description: 'Database description demo test', fontsize: 'xl:text-[110px]', fontSizeTablet: 'md:text-[65px]', fontSizeMobile: 'text-[24px]' },
-		{ name: 'Frontend', icon: frontendIcon, description: 'Frontend description demo test', fontsize: 'xl:text-[110px]', fontSizeTablet: 'md:text-[65px]', fontSizeMobile: 'text-[24px]' },
-		{ name: 'Backend', icon: backendIcon, description: 'Backend description demo test', fontsize: 'xl:text-[120px]', fontSizeTablet: 'md:text-[70px]', fontSizeMobile: 'text-[26px]' }
+	const icons: {
+		name: string;
+		icon: string;
+		description: string;
+		fontsize?: string;
+		fontSizeMobile?: string;
+		fontSizeTablet?: string;
+	}[] = [
+		{
+			name: 'UX/UI',
+			icon: uxuiIcon,
+			description: 'ถ้าชอบการออกแบบและการแก้ปัญหาให้คนอื่น สายนี้ถือตอบโจทย์',
+			fontsize: 'xl:text-[140px]',
+			fontSizeTablet: 'md:text-[100px]',
+			fontSizeMobile: 'text-[30px]'
+		},
+		{
+			name: 'Database',
+			icon: databaseIcon,
+			description:
+				'ฝ่าย Database จะพาไปเรียนรู้ตั้งแต่พื้นฐานการจัดการข้อมูล ไปจนถึงการนำข้อมูลไปใช้จริง พร้อม Workshop ให้ได้ลองทำจริง',
+			fontsize: 'xl:text-[110px]',
+			fontSizeTablet: 'md:text-[65px]',
+			fontSizeMobile: 'text-[24px]'
+		},
+		{
+			name: 'Frontend',
+			icon: frontendIcon,
+			description: 'ทำไมบางเว็บไซต์ถึงดูสวย ลื่นไหล และน่าใช้งานตั้งแต่ครั้งแรกที่เปิด?',
+			fontsize: 'xl:text-[110px]',
+			fontSizeTablet: 'md:text-[65px]',
+			fontSizeMobile: 'text-[24px]'
+		},
+		{
+			name: 'Backend',
+			icon: backendIcon,
+			description:
+				'สำหรับน้องๆทุกคนที่ เขียนโค้ดไม่คล่อง มนุษย์ต่างดาวมาจีบ หรือ ไดโนเสาร์มาบุกบ้านก็ไม่ต้องกลัว พี่ๆพร้อม',
+			fontsize: 'xl:text-[120px]',
+			fontSizeTablet: 'md:text-[70px]',
+			fontSizeMobile: 'text-[26px]'
+		}
 	];
-    
-    const sponsors = ['SPONSOR1', 'SPONSOR2', 'SPONSOR3', 'SPONSOR4', 'SPONSOR5', 'SPONSOR6', 'SPONSOR7', 'SPONSOR8'];
+
+	const sponsors = [
+		'SPONSOR1',
+		'SPONSOR2',
+		'SPONSOR3',
+		'SPONSOR4',
+		'SPONSOR5',
+		'SPONSOR6',
+		'SPONSOR7',
+		'SPONSOR8'
+	];
 
 	const carouselIcon = (dir: number) => {
 		currentIcon = (currentIcon + dir + icons.length) % icons.length;
@@ -25,5 +70,3 @@
 
 <DesktopAbout {sponsors} {currentIcon} {carouselIcon} {icons} />
 <MobileAbout {sponsors} {currentIcon} {carouselIcon} {icons} />
-
-
